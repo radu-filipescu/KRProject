fa7358d3ae397ce1491abedb42ade44c6673547d
radu-filipescu <radu.filipescu@s.unibuc.ro>
2022-05-31 02:02:48 +0300

    done with data input

--- main.py
+++ main.py
@@ -1,8 +1,68 @@
 ### Filipescu Radu - 251
 ### Micul Vrajitor
 
+import numpy as np
+
 ### reading input data
+from collections import defaultdict
 
-input_filepath = "input.txt"
+INPUT_FILEPATH = "input.txt"
+OUTPUT_FILEPATH = "output.txt"
+NSOL = 1
+TIMEOUT_TIME = 4000
 
-## TO DO 
+## TO DO - input parameters from console
+
+def defFactory():
+    return 0
+color_cost_map = defaultdict(defFactory)
+
+input_file = open(INPUT_FILEPATH, "rt", encoding="utf-8")
+
+input_line = input_file.readline()
+while input_line != "----\n":
+    input_line = input_line.split()
+    color_cost_map[input_line[0]] = input_line[1]
+    input_line = input_file.readline()
+
+color_matrix = []
+
+input_line = input_file.readline()
+while input_line != "\n":
+    input_line = input_line.split()
+
+    row = []
+    for x in input_line:
+        row.append(x)
+
+    color_matrix.append(row)
+    input_line = input_file.readline()
+
+# using numpy for faster access and manipulation
+color_matrix = np.array(color_matrix)
+
+boots_matrix = []
+
+input_line = input_file.readline()
+while input_line != "":
+    input_line = input_line.split()
+
+    row = []
+    for x in input_line:
+        row.append(x)
+
+    boots_matrix.append(row)
+    input_line = input_file.readline()
+
+boots_matrix = np.array(boots_matrix)
+
+START_POSITION = (-1, -1)
+STONE_POSITION = (-1, -1)
+
+for i in range(len(boots_matrix)):
+    for j in range(len(boots_matrix[i])):
+        if boots_matrix[i][j] == '*':
+            START_POSITION = (i, j)
+        if boots_matrix[i][j] == '@':
+            STONE_POSITION = (i, j)
+
